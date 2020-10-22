b558658a7f93cee890fb8ac5067a0f85d7036068
ntlinh16 <ntlinh16@gmail.com>
2020-10-23 00:24:27 +0200

    feat: add workflow for experiment

--- examples/experiment/antidotedb/antidotedb_fmke_g5k.py
+++ examples/experiment/antidotedb/antidotedb_fmke_g5k.py
@@ -1,19 +1,19 @@
+from datetime import datetime
+from time import sleep
 import os
 import shutil
 import traceback
-from execo_g5k import utils
-import yaml
 
-from kubernetes.config import kube_config
-
-from cloudal.utils import get_logger, get_file, execute_cmd, get_remote_executor, parse_config_file, getput_file
+from cloudal.utils import get_logger, execute_cmd, parse_config_file, getput_file
 from cloudal.action import performing_actions_g5k
 from cloudal.provisioner import g5k_provisioner
-from cloudal.configurator import kubernetes_configurator
-from cloudal.configurator import docker_configurator
-from cloudal.configurator import k8s_resources_configurator
+from cloudal.configurator import kubernetes_configurator, docker_configurator, k8s_resources_configurator
+from cloudal.experimenter import create_paramsweeper, create_combination_dir
 
+from execo_g5k import get_oar_job_info, oardel
+from execo_engine import slugify
 from kubernetes import config
+import yaml
 
 logger = get_logger()
 
@@ -21,23 +21,36 @@ logger = get_logger()
 class FMKe_antidotedb_g5k(performing_actions_g5k):
     def __init__(self, **kwargs):
         super(FMKe_antidotedb_g5k, self).__init__()
+        self.args_parser.add_argument("--kube-master", dest="kube_master",
+                                      help="name of kube master node",
+                                      default='',
+                                      type=str)
 
-    def get_results(self, kube_master):
-        logger.info('Dowloading the results ......')
+    def define_parameters(self):
+        parameters = {
+            'iteration': range(1, self.configs['parameters']['iteration'] + 1),
+            'concurrent_clients': self.configs['parameters']['concurrent_clients']
+        }
+        logger.info('Parameters:\n%s' % parameters)
+        return parameters
+
+    def get_results(self, kube_master, comb):
+        logger.info("5. ----------------------")
+        logger.info("5. Starting dowloading the results")
+        result_dir = self.configs['exp_env']['results_dir']
+        comb_dir = create_combination_dir(comb, result_dir)
         cmd = """kubectl get nodes --selector="service_g5k=fmke_client" | tail -n +2 | awk '{print $1}'"""
         _, r = execute_cmd(cmd, kube_master)
         results_nodes = r.processes[0].stdout.strip().split('\r\n')
-        results_path = self.configs['exp_env']['results_dir']
-        if not os.path.exists(results_path):
-            os.mkdir(results_path)
         getput_file(hosts=results_nodes,
                     file_paths=['/tmp/results/'],
-                    dest_location=results_path,
+                    dest_location=comb_dir,
                     action='get')
+        logger.info("Finish dowloading the results")
 
-    def perform_exp(self, kube_master):
-        logger.info('==============================================================')
-        logger.info('Starting deploying fmke client to stress the Antidote database')
+    def perform_exp(self, kube_master, concurrent_clients):
+        logger.info('--------------------------------------------------------------')
+        logger.info('4. Starting deploying fmke client to stress the Antidote database')
         fmke_client_k8s_dir = self.configs['exp_env']['fmke_yaml_path']
 
         logger.debug('Delete old k8s yaml files if exists')
@@ -69,6 +82,7 @@ class FMKe_antidotedb_g5k(performing_actions_g5k):
             with open(file_path) as f:
                 doc = f.read().replace('["127.0.0.1"]', '%s' % fmke_IPs)
                 doc = doc.replace("[9090]", '%s' % fmke_ports)
+                doc = doc.replace("{concurrent, 16}.", '{concurrent, %s}.' % concurrent_clients)
                 doc = doc.replace("'", '"')
             file_path2 = os.path.join(fmke_client_k8s_dir, 'fmke_client_%s.config' % cluster)
             with open(file_path2, 'w') as f:
@@ -99,7 +113,6 @@ class FMKe_antidotedb_g5k(performing_actions_g5k):
         configurator = k8s_resources_configurator()
         configurator.deploy_k8s_resources(files=fmke_client_files, namespace=self.kube_namespace)
 
-        logger.info("Stressing database .....")
         t = '0'
         with open(os.path.join(fmke_client_k8s_dir, 'fmke_client.config.template')) as search:
             for line in search:
@@ -107,14 +120,15 @@ class FMKe_antidotedb_g5k(performing_actions_g5k):
                 if "{duration" in line:
                     t = line.split(',')[1].split('}')[0].strip()
         timeout = int(t) + 5
+        logger.info("Stressing database in %s minutes ....." % t)
         cmd = 'kubectl wait --for=condition=complete job -l "app=fmke-client" --timeout=%sm -n %s' % (
             timeout, self.kube_namespace)
         execute_cmd(cmd, kube_master)
         logger.info("Finish stressing Antidote database")
 
     def config_fmke(self, kube_master):
-        logger.info('=================================')
-        logger.info('Starting deploying FMKe benchmark')
+        logger.info('---------------------------------')
+        logger.info('3. Starting deploying FMKe benchmark')
         fmke_k8s_dir = self.configs['exp_env']['fmke_yaml_path']
 
         logger.debug('Delete old deployment files')
@@ -202,7 +216,7 @@ class FMKe_antidotedb_g5k(performing_actions_g5k):
         logger.info('Finish deploying FMKe benchmark')
 
     def config_antidote(self, kube_master):
-        logger.info('====================================')
+        logger.info('-----------------------------------')
         logger.info('Starting deploying Antidote cluster')
         antidote_k8s_dir = self.configs['exp_env']['antidote_yaml_path']
 
@@ -353,23 +367,21 @@ class FMKe_antidotedb_g5k(performing_actions_g5k):
         configurator.deploy_k8s_resources(files=deploy_files)
 
         logger.info('Waiting for setting local persistance volumes')
+        sleep(10)
         cmd = 'kubectl wait --for=condition=Ready pod -l "app.kubernetes.io/instance=local-volume-provisioner" --timeout=300s -n default'
         execute_cmd(cmd, kube_master)
 
     def _get_credential(self, kube_master):
         home = os.path.expanduser('~')
         kube_dir = os.path.join(home, '.kube')
-
         if not os.path.exists(kube_dir):
             os.mkdir(kube_dir)
-
-        get_file(host=kube_master, remote_file_paths=['~/.kube/config'], local_dir=kube_dir)
+        getput_file(hosts=[kube_master], file_paths=['~/.kube/config'], dest_location=kube_dir, action='get')
         kube_config_file = os.path.join(kube_dir, 'config')
         config.load_kube_config(config_file=kube_config_file)
         logger.info('Kubernetes config file is stored at: %s' % kube_config_file)
 
-    def config_kube(self, kube_master):
-        logger.info("=====================================")
+    def config_host(self, kube_master):
         logger.info("Starting deploying Kubernetes cluster")
         logger.debug("Init configurator: docker_configurator")
         configurator = docker_configurator(self.hosts)
@@ -396,18 +408,90 @@ class FMKe_antidotedb_g5k(performing_actions_g5k):
         logger.info("Finish deploying the Kubernetes cluster")
 
     def clean_k8s_resources(self, kube_master):
-        logger.info('Deleting all k8s resource from the previous run in namespace "%s"' % self.kube_namespace)
+        logger.info('1. Deleting all k8s resource from the previous run in namespace "%s"' % self.kube_namespace)
+        logger.debug('Delete namespace "%s" to delete all the resource, then create it again' % self.kube_namespace)
         cmd = '''kubectl config set-context --current --namespace=default &&
                 kubectl delete namespaces %s &&
                 kubectl create namespace %s  &&
                 kubectl config set-context --current --namespace=%s ''' % (self.kube_namespace, self.kube_namespace, self.kube_namespace)
         execute_cmd(cmd, kube_master)
 
-    def config_host(self, kube_master):
-        self.config_kube(kube_master)
-        self.clean_k8s_resources(kube_master)
-        self.config_antidote(kube_master)
-        self.config_fmke(kube_master)
+        logger.debug('Delete all files in /tmp/results folder on fmke_client nodes')
+        cmd = """kubectl get nodes --selector="service_g5k=fmke_client" | tail -n +2 | awk '{print $1}'"""
+        _, r = execute_cmd(cmd, kube_master)
+        results_nodes = r.processes[0].stdout.strip().split('\r\n')
+        cmd = 'rm -rf /tmp/results && mkdir -p /tmp/results'
+        execute_cmd(cmd, results_nodes)
+
+    def workflow(self, comb, kube_master):
+        comb_ok = False
+        try:
+            logger.info('=======================================')
+            logger.info('Performing combination: ' + slugify(comb))
+            self.clean_k8s_resources(kube_master)
+            self.config_antidote(kube_master)
+            self.config_fmke(kube_master)
+
+            self.perform_exp(kube_master, comb['concurrent_clients'])
+
+            self.get_results(kube_master, comb)
+            comb_ok = True
+        finally:
+            if comb_ok:
+                self.sweeper.done(comb)
+                logger.info('Finish combination: %s' % slugify(comb))
+            else:
+                self.sweeper.cancel(comb)
+                logger.warning(slugify(comb) + ' is canceled')
+            logger.info('%s combinations remaining\n' % len(self.sweeper.get_remaining()))
+            logger.info('====================================')
+
+    def setup_env(self, kube_master_site):
+        logger.info("STARTING SETTING THE EXPERIMENT ENVIRONMENT")
+        logger.info("STARTING PROVISIONING NODES")
+        logger.info("Init provisioner: g5k_provisioner")
+        provisioner = g5k_provisioner(configs=self.configs,
+                                      keep_alive=self.args.keep_alive,
+                                      out_of_chart=self.args.out_of_chart,
+                                      oar_job_ids=self.args.oar_job_ids,
+                                      no_deploy_os=self.args.no_deploy_os,
+                                      is_reservation=self.args.is_reservation,
+                                      job_name="cloudal_k8s")
+
+        provisioner.provisioning()
+        self.hosts = provisioner.hosts
+        oar_job_ids = provisioner.oar_result
+        logger.info("FINISH PROVISIONING NODES")
+
+        logger.info("STARTING CONFIGURING NODES")
+        kube_master = self.args.kube_master
+        if kube_master is None:
+            for host in self.hosts:
+                if host.startswith(kube_master_site):
+                    kube_master = host
+                    break
+
+        self.kube_namespace = 'fmke-exp'
+        if self.args.kube_master is None:
+            self.config_host(kube_master)
+        else:
+            logger.info('Kubernetes master: %s' % kube_master)
+            self._get_credential(kube_master)
+
+        logger.info("FINISH CONFIGURING NODES")
+        self.args.oar_job_ids = None
+        logger.info("FINISH SETTING THE EXPERIMENT ENVIRONMENT\n")
+        return kube_master, oar_job_ids
+
+    def is_job_alive(self, oar_job_ids):
+        for oar_job_id, site in oar_job_ids:
+            job_info = get_oar_job_info(oar_job_id, site)
+            while 'state' not in job_info:
+                job_info = get_oar_job_info(oar_job_id, site)
+                sleep(5)
+            if job_info['state'] == 'Error':
+                return False
+        return True
 
     def create_configs(self):
         logger.debug('Get the k8s master node')
@@ -430,42 +514,28 @@ class FMKe_antidotedb_g5k(performing_actions_g5k):
         return kube_master_site
 
     def run(self):
-        logger.info("STARTING PROVISIONING NODES")
         logger.debug('Parse and convert configs for G5K provisioner')
         self.configs = parse_config_file(self.args.config_file_path)
         kube_master_site = self.create_configs()
 
-        logger.info("Init provisioner: g5k_provisioner")
-        provisioner = g5k_provisioner(configs=self.configs,
-                                      keep_alive=self.args.keep_alive,
-                                      out_of_chart=self.args.out_of_chart,
-                                      oar_job_ids=self.args.oar_job_ids,
-                                      no_deploy_os=self.args.no_deploy_os,
-                                      is_reservation=self.args.is_reservation,
-                                      job_name="cloudal_k8s")
+        result_dir = self.configs['exp_env']['results_dir']
+        if not os.path.exists(result_dir):
+            os.mkdir(result_dir)
+        parameters = self.define_parameters()
+        self.sweeper = create_paramsweeper(parameters, result_dir)
 
-        provisioner.provisioning()
-        self.hosts = provisioner.hosts
-        logger.info("FINISH PROVISIONING NODES\n")
-
-        logger.info("STARTING CONFIGURING THE EXPERIMENT ENVIRONMENT")
+        oar_job_ids = None
         kube_master = None
-        for host in self.hosts:
-            if host.startswith(kube_master_site):
-                kube_master = host
-                break
+        while len(self.sweeper.get_remaining()) > 0:
+            if oar_job_ids is None:
+                kube_master, oar_job_ids = self.setup_env(kube_master_site)
 
-        self.kube_namespace = 'fmke-exp'
-        self.config_host(kube_master)
-        logger.info("FINISH CONFIGURING THE EXPERIMENT ENVIRONMENT\n")
+            comb = self.sweeper.get_next()
+            self.workflow(kube_master=kube_master, comb=comb)
 
-        logger.info("STARTING PERFORMING THE EXPERIMENTS")
-        self.perform_exp(kube_master)
-        logger.info("FINISH THE EXPERIMENTS\n")
-
-        logger.info("GETTING THE RESULTS")
-        self.get_results(kube_master)
-        logger.info("FINISH GETTING THE RESULTS")
+            if not self.is_job_alive(oar_job_ids):
+                oar_job_ids = None
+                oardel(oar_job_ids)
 
 
 if __name__ == "__main__":
@@ -481,3 +551,10 @@ if __name__ == "__main__":
         traceback.print_exc()
     except KeyboardInterrupt:
         logger.info('Program is terminated by keyboard interrupt.')
+
+    if not engine.args.keep_alive:
+        logger.info('Deleting reservation')
+        oardel(engine.provisioner.oar_result)
+        logger.info('Reservation deleted')
+    else:
+        logger.info('Reserved nodes are kept alive for inspection purpose.')
