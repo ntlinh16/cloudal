7e7dac34f1eb4eeb688f0db2eea19d1b64be00b2
ntlinh16 <ntlinh16@gmail.com>
2020-10-23 14:42:25 +0200

    feat: add k8s wait function to get a resource ready/complete

--- cloudal/configurator/k8s_resources_configurator.py
+++ cloudal/configurator/k8s_resources_configurator.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-from cloudal.utils import get_logger
+from cloudal.utils import get_logger, execute_cmd
 
 from kubernetes import utils
 from kubernetes.utils import FailToCreateError
@@ -34,3 +34,39 @@ class k8s_resources_configurator(object):
                 for api_exception in e.api_exceptions:
                     body = json.loads(api_exception.body)
                     logger.error('Error: %s, because: %s' % (api_exception.reason, body['message']))
+
+    def wait_k8s_resources(self, resource, label_selectors,
+                           kube_master, kube_namespace='default', timeout='60s', is_continue=False):
+        '''Wait until specified k8s resources are completed or ready
+
+        Parameters
+        ----------
+        resource: string
+            the name of the resource (job, pod, etc.)
+
+        label_selectors: string
+            the k8s labels used to filter to resource, the format is: key1=value1,key2=value2,...
+
+        kube_master: string
+            the hostname of the kube master node
+
+        kube_namespace: string
+            the k8s namespace to perform the wait of k8s resources operation on,
+            the default namespace is 'default'
+
+        timeout: string
+            the length of time to wait before giving up, the format looks like: 60s, 1m, etc.
+
+        is_continue: bool
+            when set to True no exception raises even if the wait operation fails,
+            raise exception otherwise
+        '''
+        wait_condition = 'complete' if resource == 'job' else 'Ready'
+        cmd = 'kubectl wait --for=condition={condition} {resource} -l "{labels}" --timeout={timeout} -n {namespace}'.format(
+            condition=wait_condition,
+            resource=resource,
+            labels=label_selectors,
+            timeout=timeout,
+            namespace=kube_namespace
+        )
+        execute_cmd(cmd, kube_master, is_continue=is_continue)
