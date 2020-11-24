13091dea197118e0fe13c5b8e0ecca3e0722282a
ntlinh16 <ntlinh16@gmail.com>
2020-11-24 12:34:47 +0100

    doc: fix typo

--- cloudal/configurator/docker_swarm_configurator.py
+++ cloudal/configurator/docker_swarm_configurator.py
@@ -11,35 +11,35 @@ class docker_swarm_configurator(object):
 
     def __init__(self, hosts, ds_manager=None):
         self.hosts = hosts
-        self.ds_maneger = ds_manager
+        self.ds_manager = ds_manager
 
     def deploy_docker_swarm_cluster(self):
         configurator = docker_configurator(self.hosts)
         configurator.config_docker()
 
-        if self.ds_maneger is None:
-            self.ds_maneger = self.hosts[0]
+        if self.ds_manager is None:
+            self.ds_manager = self.hosts[0]
             ds_workers = self.hosts[1:]
         else:
-            ds_workers = [host for host in self.hosts if host != self.ds_maneger]
+            ds_workers = [host for host in self.hosts if host != self.ds_manager]
 
-        logger.info('Getting IP of docker swarm maneger')
+        logger.info('Getting IP of docker swarm manager')
         cmd = "hostname -I"
-        _, r = execute_cmd(cmd, [self.ds_maneger])
-        ds_maneger_ip = r.processes[0].stdout.strip().split(' ')[0]
+        _, r = execute_cmd(cmd, [self.ds_manager])
+        ds_manager_ip = r.processes[0].stdout.strip().split(' ')[0]
 
         logger.info('Creating a new swarm')
-        cmd = 'docker swarm init --advertise-addr %s' % ds_maneger_ip
-        execute_cmd(cmd, [self.ds_maneger])
+        cmd = 'docker swarm init --advertise-addr %s' % ds_manager_ip
+        execute_cmd(cmd, [self.ds_manager])
 
         logger.info('Joining all docker swarm worker')
         cmd = 'docker swarm join-token worker | grep "docker swarm join"'
-        _, r = execute_cmd(cmd, [self.ds_maneger])
+        _, r = execute_cmd(cmd, [self.ds_manager])
 
         cmd = r.processes[0].stdout.strip()
         execute_cmd(cmd, ds_workers)
 
         logger.info('Finish deploying docker swarm cluster')
-        logger.info('Docker swarm maneger: %s (%s)' % (self.ds_maneger, ds_maneger_ip))
+        logger.info('Docker swarm manager: %s (%s)' % (self.ds_manager, ds_manager_ip))
 
-        return self.ds_maneger, ds_workers
+        return self.ds_manager, ds_workers
